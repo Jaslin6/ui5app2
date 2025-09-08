cbf040f52be35953b22b5c1ebab4f72c590eff36
Jaslin Reena <jaslin@fscmfactory.com>
2025-09-08 04:16:01 +0000

    save_1

--- webapp/controller/Root.controller.js
+++ webapp/controller/Root.controller.js
@@ -1,10 +1,27 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-], (Controller) => {
+], function(Controller) {
     "use strict";
 
     return Controller.extend("ui5app2.controller.Root", {
-        onInit() {
-        }
+        onInit: function() {
+        },
+
+        onOpenDialog: function(){
+
+            if(!this.moreBankDetails){
+                this.moreBankDetails = this.loadFragment({
+                    name:"ui5app2.view.fragments.MoreDetails"
+                
+                });
+
+                this.moreBankDetails.then(function(oDialog){
+                    oDialog.open();
+                })
+            }
+        },
+        onCloseDialog: function(){
+            this.byId("Dialog").close();
+        }     
     });
 });
